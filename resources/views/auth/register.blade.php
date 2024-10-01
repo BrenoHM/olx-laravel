989c9b5c599954a7bea29d7d15c25863564630d3
Breno Mol <emaildobrenomol@gmail>
2024-10-01 08:24:00 -0300

    B7Store - Registro 1/2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,26 +10,27 @@
   </head>
 
   <body>
-    <a href="index.html" class="back-button">← Voltar</a>
+    <a href="{{ route('login') }}" class="back-button">← Voltar</a>
     <div class="login-page">
       <div class="login-area">
         <h3 class="login-title">B7Store</h3>
         <div class="text-login">
           Preencha os campos abaixo e realize seu cadastro.
         </div>
-        <form>
+        <form method="POST" action="{{ route('register_action') }}">
+          @csrf
           <div class="name-area">
             <div class="name-label">Nome</div>
-            <input type="text" placeholder="Digite o seu nome" />
+            <input type="text" name="name" placeholder="Digite o seu nome" />
           </div>
           <div class="email-area">
             <div class="email-label">E-mail</div>
-            <input type="email" placeholder="Digite o seu e-mail" />
+            <input type="email" name="email" placeholder="Digite o seu e-mail" />
           </div>
           <div class="password-area">
             <div class="password-label">Senha</div>
             <div class="password-input-area">
-              <input type="password" placeholder="Digite a sua senha" />
+              <input type="password" name="password" placeholder="Digite a sua senha" />
               <img src="assets/icons/eyeIcon.png" alt="Ícone mostrar senha" />
             </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'register_action'])->name('register_action');
 
 Route::get('/login', function () {
     return view('auth.login');
