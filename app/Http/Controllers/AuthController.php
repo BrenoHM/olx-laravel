4fc5789bc28fb11c74f38517b0f91e3f5920141b
Breno Mol <emaildobrenomol@gmail>
2024-10-07 09:24:53 -0300

    B7Store - Criando o Usuário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -13,6 +16,16 @@ class AuthController extends Controller
     }
 
     public function register_action(RegisterRequest $request)
+    
+    {
+        $userData = $request->validated();
+        $userData['password'] = Hash::make($userData['password']);
+        $user = User::create($userData);
+        Auth::loginUsingId($user->id);
+    }
+
+    public function state_action(Request $request)
+    
     {
         dd($request->all());
     }
---
+++ resources/views/auth/select-state.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="stylesheet" href="assets/loginSignUpStyle.css" />
+
+    <title>B7Store - Selecione seu estado</title>
+  </head>
+
+  <body>
+    <a href="{{ route('login') }}" class="back-button">← Voltar</a>
+    <div class="login-page">
+      <div class="login-area">
+        <h3 class="login-title">B7Store</h3>
+        <div class="text-login">
+          Selecione o seu estado
+        </div>
+        <form method="POST" action="{{ route('state_action') }}">
+          @csrf
+          <div class="name-area">
+            <div class="name-label">Estado</div>
+            <input type="text" name="name" placeholder="Digite o seu nome" @error('name') class="is-invalid" @enderror />
+            @error('name')
+              <div class="error">{{ $message }}</div>
+            @enderror
+          </div>
+          <button class="login-button">Selecionar</button>
+        </form>
+        {{-- <div class="register-area">
+          Já tem cadastro? <a href="{{ route('login') }}">Fazer Login</a>
+        </div> --}}
+      </div>
+      {{-- <div class="terms">
+        Ao continuar, você concorda com os <a href="">Termos de Uso</a> e a
+        <a href="">Política de Privacidade</a>, e também, em receber
+        comunicações via e-mail e push de todos os nossos parceiros.
+      </div> --}}
+    </div>
+    <x-base.footer />
+  </body>
+</html>
