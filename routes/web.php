eb47fe7d40e8500bc70f353f04f7d722bb8c72f1
Breno Mol <emaildobrenomol@gmail>
2024-10-23 08:57:52 -0300

    B7Store - Rota de logout e Middleware de Auth

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
-
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'register_action'])->name('register_action');
-
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
-
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
 Route::post('/login', [AuthController::class, 'login_action'])->name('loginAction');
-
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/forgot-password', function () {
     return view('auth.forgot-password');
 })->name('forgot-password');
-
 Route::get('/select-state', [AuthController::class, 'select_state'])->name('select-state');
-
 Route::post('/select-state', [AuthController::class, 'state_action'])->name('state_action');
-
-Route::get('/dashboard/my-account', [DashboardController::class, 'my_account'])->name('my_account');
-Route::post('/dashboard/my-account', [DashboardController::class, 'action_my_account'])->name('action.my_account');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard/my-account', [DashboardController::class, 'my_account'])->name('my_account');
+    Route::post('/dashboard/my-account', [DashboardController::class, 'action_my_account'])->name('action.my_account');
+});
