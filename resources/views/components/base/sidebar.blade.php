d1c02a05106089664686d0f2979bd908cf9ef732
Breno Mol <emaildobrenomol@gmail>
2024-10-24 14:54:26 -0300

    B7Store - Criando a tela "Meus Anuncios"

--- resources/views/components/base/sidebar.blade.php
+++ resources/views/components/base/sidebar.blade.php
@@ -1,7 +1,19 @@
 <div class="sidebar">
     <div class="sidebar-top">
-        <a href="{{ route('my_account') }}" class="config"><img src="/assets/icons/configIcon.png" /> Configurações</a>
-        <a href="{{ route('my_ads') }}" ><img src="/assets/icons/layersIonGray.png" /> Meus Anúncios</a>
+        <a href="{{ route('my_account') }}" class="{{ request()->is('dashboard/my-account') ? 'config' : ''}}">
+            @if(request()->is('dashboard/my-account')) 
+                <img src="/assets/icons/configIcon.png" />
+            @else
+                <img src="/assets/icons/configIconGray.png" />
+            @endif Configurações
+        </a>
+        <a href="{{ route('my_ads') }}" class="{{ request()->is('dashboard/my-ads') ? 'config' : ''}}">
+            @if(request()->is('dashboard/my-ads')) 
+                <img src="/assets/icons/layersIcon.png" />
+            @else
+                <img src="/assets/icons/layersIconGray.png" />
+            @endif Meus Anúncios
+        </a>
     </div>
     <div class="sidebar-bottom">
         <a href="{{ route('logout') }}"><img src="/assets/icons/logoutIcon.png" /> Sair</a>
