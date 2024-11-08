ca71f4c98360e592cd965390e5121c4cddbf4bee
Breno Mol <emaildobrenomol@gmail>
2024-11-08 08:47:11 -0300

    Deletando imagens relacionadas ao Ad

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 class AdController extends Controller
 {
     //
-    public function delete($id)
+    public function delete(String $id)
     {
-        dd($id);
-        return view('dashboard.my-ads');
+        $ad = auth()->user()->advertises()->where('id', $id)->first();
+        if(!$ad) {
+            return redirect()->back()->with('error', 'Anúncio não encontrado.');
+        }
+        $ad->images()->delete();
+        $ad->delete();
+        return redirect()->back()->with('success', 'Anúncio excluído com sucesso.');
     }
 }
