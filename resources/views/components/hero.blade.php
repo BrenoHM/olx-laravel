735700014c991d5c69a6f9c489a7005a4724389d
Breno Mol <emaildobrenomol@gmail>
2024-12-16 08:43:49 -0300

    Ajustes visuais no código HTML e CSS

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,27 +1,25 @@
 <section class="hero-area">
     <h3 class="subtitle">Aqui você encontra o que tanto procura!</h3>
     <h1 class="title">O que você está procurando?</h1>
-    <div class="search-area">
-      <form action="{{route('ad.list')}}">
-        <input
-          class="search-text"
-          name="s"
-          type="text"
-          placeholder="Estou procurando por..."
-        />
-        <select name="c" class="categories-options">
-          <option selected hidden disabled value="">Categoria</option>
-          @foreach ($categories as $category)
-            <option value="{{ $category['value'] }}">{{ $category['label'] }}</option>
-          @endforeach
-        </select>
-        <select name="st" class="states">
-          <option selected hidden disabled value="">Estado</option>
-          @foreach ($states as $state)
-            <option value="{{ $state['value'] }}">{{ $state['label'] }}</option>
-          @endforeach
-        </select>
-        <button type="submit" class="search-button">Procurar</button>
-      </form>
-    </div>
+    <form class="search-area" action="{{route('ad.list')}}">
+      <input
+        class="search-text"
+        name="s"
+        type="text"
+        placeholder="Estou procurando por..."
+      />
+      <select name="c" class="categories-options">
+        <option selected hidden disabled value="">Categoria</option>
+        @foreach ($categories as $category)
+          <option value="{{ $category->id }}">{{ $category->name }}</option>
+        @endforeach
+      </select>
+      <select name="st" class="states">
+        <option selected hidden disabled value="">Estado</option>
+        @foreach ($states as $state)
+          <option value="{{ $state->id }}">{{ $state->name }}</option>
+        @endforeach
+      </select>
+      <button type="submit" class="search-button">Procurar</button>
+    </form>
 </section>
