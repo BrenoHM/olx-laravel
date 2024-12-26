321e9ac0e10615abfccfdf933695b14ee17fffb2
Breno Mol <emaildobrenomol@gmail>
2024-12-26 09:36:33 -0300

    Componentizando com Livewire o Novo Ad

--- app/Livewire/AdCreate.php
+++ app/Livewire/AdCreate.php
@@ -2,12 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class AdCreate extends Component
 {
+    public $title;
+    public $description;
+    public $price;
+    public $category_id;
+    public $negotiable;
+    //public $image;
+
     public function render()
     {
-        return view('livewire.ad-create');
+        $categories = Category::all();
+        return view('livewire.ad-create', compact('categories'));
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        // Save the ad to the database
+
+        session()->flash('message', 'Ad created successfully.');
+
+        return redirect()->to('/ads');
     }
 }
